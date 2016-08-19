e22c3f9385fa78db5cebe9acdd8b466e6f658113
Ben Ubois <ben@benubois.com>
2016-08-18 20:38:20 -0700

    FeedRequest tests.

--- Rakefile
+++ Rakefile
@@ -1,12 +1,15 @@
 require 'rake/testtask'
 
+path = File.expand_path(File.dirname(__FILE__))
+
 Rake::TestTask.new(:test) do |test|
+  test.libs = []
+  test.ruby_opts = ['-I', path, '-r', './app/boot.rb']
   test.pattern = 'test/**/*_test.rb'
 end
 
 desc "Open an irb session preloaded with this library"
 task :console do
-  path = File.expand_path(File.dirname(__FILE__))
   sh "irb -rubygems -I #{path} -r ./app/boot"
 end
 
--- test/feed_request_test.rb
+++ test/feed_request_test.rb
@@ -1,6 +1,4 @@
-require 'zlib'
 require_relative 'test_helper'
-require_relative '../app/models/feed_request'
 
 class TestFeedRequest < Minitest::Test
 
@@ -15,7 +13,7 @@ class TestFeedRequest < Minitest::Test
   end
 
   def test_get_gzipped_body
-    url = "http://www.example.com/atom.xml"
+    url = "http://www.example.com"
     body = random_string
 
     response = {
@@ -29,6 +27,85 @@ class TestFeedRequest < Minitest::Test
     feed_request = FeedRequest.new(url: url)
 
     assert_equal body, feed_request.body
+  end
+
+  def test_should_be_xml
+    url = "http://www.example.com/atom.xml"
+    stub_request_file("atom.xml", url)
+    feed_request = FeedRequest.new(url: url)
+    assert_equal :xml, feed_request.format
+  end
+
+  def test_should_be_html
+    url = "http://www.example.com/atom.xml"
+    body = random_string
+    stub_request(:get, url).to_return(body: body)
+    feed_request = FeedRequest.new(url: url)
+    assert_equal :html, feed_request.format
+  end
+
+  def test_should_follow_redirects
+    first_url = "http://www.example.com"
+    last_url = "#{first_url}/final"
+    body = random_string
+
+    response = {
+      status: 301,
+      headers: {
+        "Location" => last_url
+      }
+    }
+    stub_request(:get, first_url).to_return(response)
+    stub_request(:get, last_url)
+
+    feed_request = FeedRequest.new(url: first_url)
+    assert_equal last_url, feed_request.last_effective_url
+  end
+
+  def test_should_get_caching_headers
+    url = "http://www.example.com/atom.xml"
+    last_modified = Time.now
+    etag = random_string
+
+    response = {
+      headers: {
+        "Last-Modified" => last_modified.httpdate,
+        "Etag" => etag
+      }
+    }
+    stub_request(:get, url).to_return(response)
+    feed_request = FeedRequest.new(url: url)
+
+    assert_equal last_modified.httpdate, feed_request.last_modified.httpdate
+    assert_equal etag, feed_request.etag
+  end
+
+  def test_should_not_be_modified_etag
+    url = "http://www.example.com"
+    etag = random_string
+    status = 304
+
+    request = {
+      headers: {"If-None-Match" => etag}
+    }
+    stub_request(:get, url).with(request).to_return(status: status)
+    feed_request = FeedRequest.new(url: url, options: {if_none_match: etag})
+
+    assert_equal status, feed_request.status
+  end
+
+  def test_should_not_be_modified_last_modified
+    url = "http://www.example.com"
+    last_modified = Time.now
+    status = 304
+
+    request = {
+      headers: {"If-Modified-Since" => last_modified.httpdate}
+    }
+    stub_request(:get, url).with(request).to_return(status: status)
+    feed_request = FeedRequest.new(url: url, options: {if_modified_since: last_modified})
+
+    assert_equal status, feed_request.status
   end
 
   private
