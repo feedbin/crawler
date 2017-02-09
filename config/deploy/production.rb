b379aa64ae4702546345df9c0522a201ef031fbb
Ben Ubois <ben@benubois.com>
2017-02-09 10:33:13 -0800

    Update ruby.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,1 @@
-server 'refresher1.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher2.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher3.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher4.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher5.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher6.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher7.feedbin.com', user: 'app', roles: %w{refresher}
-server 'refresher8.feedbin.com', user: 'app', roles: %w{refresher}
+server 'refresh', user: 'app', roles: %w{refresher}
