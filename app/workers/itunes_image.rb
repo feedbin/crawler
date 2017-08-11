b0a48c422b500589db852c64ba50dbe336d74d2e
Ben Ubois <ben@benubois.com>
2017-08-11 11:23:28 -0700

    Audio image updates.

--- app/workers/itunes_image.rb
+++ app/workers/itunes_image.rb
@@ -3,13 +3,46 @@ class ItunesImage
   sidekiq_options queue: :images, retry: false
 
   def perform(entry_id, image_url)
-    image = DownloadImage.new(URI(image_url), false)
 
-    if image.file
-      processed_image = ProcessedItunesImage.new(image.file)
+    processed_url = cached_value(image_url)
+
+    if !processed_url
+      response = HTTP.get image_url
+      path = Pathname.new(File.join(Dir.tmpdir, "#{SecureRandom.hex}.bin")).tap do |path|
+        File.open path, "wb" do |io|
+          while (chunk = response.readpartial)
+            io << chunk
+          end
+        end
+      end
+      processed_image = ProcessedItunesImage.new(path)
       processed_image.process
-      puts processed_image.url
+      processed_url = processed_image.url
+      if processed_url
+        cache(image_url, processed_url)
+      end
     end
+
+    if processed_url
+      Sidekiq::Client.push(
+        'args'  => [entry_id, nil, processed_url],
+        'class' => 'EntryImage',
+        'queue' => 'default'
+      )
+    end
+
+  end
+
+  def cached_value(url)
+    $redis.get(cache_key(url))
+  end
+
+  def cache(url, processed_url)
+    $redis.set(cache_key(url), processed_url)
+  end
+
+  def cache_key(url)
+    "itunes_image:#{Digest::SHA1.hexdigest(url)}"
   end
 
 end
