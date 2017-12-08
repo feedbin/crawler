fcd068879ae89c2e57baf1d907513312bbb82755
Ben Ubois <ben@benubois.com>
2017-12-08 13:06:27 -0800

    Set Referer header.

--- app/models/download_image.rb
+++ app/models/download_image.rb
@@ -9,7 +9,9 @@ class DownloadImage
       file = nil
       options = {use_ssl: @url.scheme == "https", open_timeout: 5, read_timeout: 30}
       Net::HTTP.start(@url.host, @url.port, options) do |http|
-        http.request_get(@url.request_uri) do |response|
+        request = Net::HTTP::Get.new(@url.request_uri)
+        request['Referer'] = "https://feedbin.com/"
+        http.request(request) do |response|
           if headers_valid?(response.to_hash)
             file = download_image(response)
           end
