aa38a9d234926f8cf258a66b401a3e0dd740867e
Ben Ubois <ben@benubois.com>
2018-01-12 04:18:59 -0800

    Separate queue for priority refershes.

---
+++ app/workers/twitter_feed_refresher_critical.rb
@@ -0,0 +1,7 @@
+class TwitterFeedRefresherCritical
+  include Sidekiq::Worker
+  sidekiq_options queue: :feed_refresher_fetcher_critical
+  def perform(*args)
+    TwitterFeedRefresher.new().perform(*args)
+  end
+end
