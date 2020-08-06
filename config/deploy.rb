34c3bf0502ca795a884c05e905233c4fb34d349d
Ben Ubois <ben@benubois.com>
2020-08-06 17:09:46 +0200

    Deploy updates.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,5 +9,39 @@ set :deploy_to, "/srv/apps/#{fetch(:application)}"
 set :bundle_jobs, 4
 set :log_level, :info
 
+namespace :deploy do
+  desc "Pause Sidekiq"
+  task :quiet do
+    on roles :all do
+      execute :sudo, :systemctl, :reload, "refresher.target"
+    rescue SSHKit::Command::Failed
+      puts "No workers running"
+    end
+  end
+
+  desc "Restart refresher processes"
+  task :restart do
+    on roles :all do
+      execute :sudo, :systemctl, :restart, "refresher.target"
+    rescue SSHKit::Command::Failed
+      execute :sudo, :systemctl, :start, "refresher.target"
+    end
+  end
+
+  desc "Stop services"
+  task :stop_bg do
+    on roles :all do
+      invoke "deploy:quiet"
+
+      sleep(10)
+
+      begin
+        execute :sudo, :stop, :workers
+      rescue SSHKit::Command::Failed
+      end
+    end
+  end
+end
+
 before "deploy", "deploy:quiet"
 after "deploy:published", "deploy:restart"
