a97b6b212fb166089be13ae0fbc05c7c505db49f
Ben Ubois <ben@benubois.com>
2021-03-31 16:05:19 -0700

    Deploy update.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,2 @@
 server "refresher1.feedbin", user: "app", roles: %w[refresher]
 server "refresher2.feedbin", user: "app", roles: %w[refresher]
-server "refresher3.feedbin", user: "app", roles: %w[refresher]
