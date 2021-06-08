c0d74424656fdaeccfdc4a7afd6ae67ecefdce72
Ben Ubois <ben@benubois.com>
2021-06-07 17:36:24 -0700

    Add Librato.

---
+++ app/worker_stat.rb
@@ -0,0 +1,9 @@
+class WorkerStat
+  def call(worker, item, queue)
+    title = "worker.#{worker.class}"
+    Librato.increment "#{title}.count"
+    Librato.timing title, percentile: [95] do
+      yield
+    end
+  end
+end
