bebf384e0a7d6577c6e75a9b2aadf0afc4cc2c8a
Ben Ubois <ben@benubois.com>
2022-08-24 11:51:50 +0200

    Renamed files.

--- test/image_test.rb
+++ test/image_test.rb
@@ -4,7 +4,7 @@ module Crawler
     class ImageTest < Minitest::Test
       def test_should_get_image_size
         file = File.expand_path("support/www/image.jpeg", __dir__)
-        image = Image.new(file, target_width: 542, target_height: 304)
+        image = ImageProcessor.new(file, target_width: 542, target_height: 304)
         assert_equal(image.width, 640)
         assert_equal(image.height, 828)
         assert_equal(542, image.resized.width)
@@ -13,14 +13,14 @@ module Crawler
 
       def test_should_get_face_location
         file = support_file("image.jpeg")
-        image = Image.new(file, target_width: 542, target_height: 304)
+        image = ImageProcessor.new(file, target_width: 542, target_height: 304)
 
         assert_equal(462, image.average_face_position("y", File.new(file)))
       end
 
       def test_should_crop
         file = File.expand_path("support/www/image.jpeg", __dir__)
-        image = Image.new(file, target_width: 542, target_height: 304)
+        image = ImageProcessor.new(file, target_width: 542, target_height: 304)
         cropped_path = image.smart_crop
         assert cropped_path.include?(".jpg")
         FileUtils.rm cropped_path
@@ -28,7 +28,7 @@ module Crawler
 
       def test_should_return_same_size_image
         file = File.expand_path("support/www/image.jpeg", __dir__)
-        image = Image.new(file, target_width: 640, target_height: 828)
+        image = ImageProcessor.new(file, target_width: 640, target_height: 828)
         cropped_path = image.smart_crop
         assert cropped_path.include?(".jpg")
       end
